4a3957c1ff2cb3e728b907bae5f747a720de0035
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2025-04-01 18:32:57 +0200

    Add CRUD to ProductController + endpoints

--- play-scala/app/controllers/ProductController.scala
+++ play-scala/app/controllers/ProductController.scala
@@ -4,13 +4,14 @@ import play.api.mvc._
 import play.api.libs.json._
 import models.Product
 import javax.inject._
+import scala.collection.mutable.ListBuffer
 
 @Singleton
 class ProductController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   implicit val productFormat: OFormat[Product] = Json.format[Product]
 
-  private val products = List(
+  private val products = ListBuffer(
     Product(1, "PC", 6999.99),
     Product(2, "Smartphone", 599.99),
     Product(3, "TV", 3999.99)
@@ -18,5 +19,47 @@ class ProductController @Inject()(cc: ControllerComponents) extends AbstractCont
 
   def getAllProducts: Action[AnyContent] = Action {
     Ok(Json.toJson(products))
+  }
+
+  def getProduct(id: Int): Action[AnyContent] = Action {
+    products.find(_.id == id) match {
+      case Some(product) => Ok(Json.toJson(product))
+      case None => NotFound(Json.obj("error" -> s"Product with ID $id not found"))
+    }
+  }
+
+  def createProduct: Action[JsValue] = Action(parse.json) { request =>
+    request.body.validate[Product] match {
+      case JsSuccess(product, _) =>
+        products.append(product)
+        Created(Json.toJson(product))
+      case JsError(errors) =>
+        BadRequest(Json.obj("error" -> "Invalid product data"))
+    }
+  }
+
+  def updateProduct(id: Int): Action[JsValue] = Action(parse.json) { request =>
+    request.body.validate[Product] match {
+      case JsSuccess(updatedProduct, _) =>
+        val productIndex = products.indexWhere(_.id == id)
+        if (productIndex >= 0) {
+          products(productIndex) = updatedProduct
+          Ok(Json.toJson(updatedProduct))
+        } else {
+          NotFound(Json.obj("error" -> s"Product with ID $id not found"))
+        }
+      case JsError(errors) =>
+        BadRequest(Json.obj("error" -> "Invalid product data"))
+    }
+  }
+
+  def deleteProduct(id: Int): Action[AnyContent] = Action {
+    val productIndex = products.indexWhere(_.id == id)
+    if (productIndex >= 0) {
+      products.remove(productIndex)
+      NoContent
+    } else {
+      NotFound(Json.obj("error" -> s"Product with ID $id not found"))
+    }
   }
 }
